77e5197950289039152fd3a1c43d7c58065aab0a
shubham <shubu01001@gmail.com>
2023-12-12 16:48:51 +0530

    first change

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 12345;
+let accountEmail = "dhiman@gmail.com"
+var accountPassword = "nothing"
+accountCity = "dehradun"
+
+//const value can't be changed
+// accountId = "23415";
+console.table([accountEmail, accountId, accountPassword, accountId]);
+// accountEmail = "shubham@gmail.com";
+// accountPassword = "something";
+// accountCity = "roorkee"
+
+
+// console.log(accountId);
+
