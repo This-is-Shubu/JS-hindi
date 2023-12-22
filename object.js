146e917d269d93dc33f4338ad75568e26a33c669
shubu <shubu01001@gmail.com>
2023-12-23 00:37:57 +0530

    first object created

--- object.js
+++ object.js
@@ -1,16 +1,23 @@
-const newObject = {
-    name: "rajneesh",
-    age: 28,
-    work: "bakchodi",
-    cheli: 5
+// const newObject = {
+//     name: "rajneesh",
+//     age: 28,
+//     work: "bakchodi",
+//     cheli: 5
+// }
+
+// console.log(newObject.cheli);
+
+// Object.freeze(newObject)
+
+// newObject.name = "change"
+// newObject.age = 29
+
+// console.log(newObject.indexOf);
+// console.log(newObject.age);
+
+const firstObject = {
+    name: "anonymous",
+    age: 26,
+    work: "operator",
+    marks: [63, 45, 76, 59, 64]
 }
-
-console.log(newObject.cheli);
-
-Object.freeze(newObject)
-
-newObject.name = "change"
-newObject.age = 29
-
-console.log(newObject.indexOf);
-console.log(newObject.age);
