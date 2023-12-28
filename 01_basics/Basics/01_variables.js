80fb0a8872a5529b93d874da15646adfa2bd1f93
shubu <shubu01001@gmail.com>
2023-12-28 23:54:18 +0530

    folder name changed to basics, old files added in basics

--- 01_basics/Basics/01_variables.js
+++ 01_basics/Basics/01_variables.js
@@ -3,7 +3,8 @@ let accountEmail = "dhiman@gmail.com"
 var accountPassword = "nothing"
 accountCity = "dehradun"
 
-//const value can't be changed
+/* const value can't be changed because of block scope and functional scope
+*/
 // accountId = "23415";
 
 accountEmail = "shubham@gmail.com";
